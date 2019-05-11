e8ded425da4f485d4b461e6910d79a8c27eade6f
Benjamin Slater <ben@hackcapital.com>
2019-05-10 23:45:24 -0700

    Got traits working, bug with new_pot lifetime.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,21 @@
 extern crate rettle;
 
-use rettle::{Pot, Ingredient, FillStruct};
+use rettle::{Pot, Fill, Steep, Pour, Brewer};
 
 fn main() {
     let mut new_pot = Pot::new();
-    new_pot.add(Ingredient::Fill(FillStruct));
-    new_pot.add(Ingredient::Steep);
-    new_pot.add(Ingredient::Pour);
-    let count = new_pot.brew();
+    new_pot.add_source(Box::new(Fill{
+        name: String::from("dummy_txt"),
+        source: String::from("hardcoded"),
+    }));
+    new_pot.add_ingredient(Box::new(Steep{
+        name: String::from("steep1")
+    }));
+    new_pot.add_ingredient(Box::new(Pour{
+        name: String::from("pour1")
+    }));
+    let new_brewer = Brewer::new(new_pot.get_recipe());
+    //new_brewer.update_steps(new_pot.get_recipe());
+    let count = new_pot.brew(new_brewer);
     println!("Count: {}", count);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,117 +1,206 @@
 use serde::{Deserialize, Serialize};
+use std::any::Any;
+//use std::fmt;
 //use serde_json::Result;
 
-#[derive(Debug, PartialEq)]
 /// Data Structure that holds the recipe to brew tea (ETL data).
-pub struct Pot<'a, T: Ingredient> {
-    recipe: Vec<Box<T>>,
-    tea: Tea<'a>,
+pub struct Pot<'a> {
+    recipe:  Vec<Box<dyn Ingredient<'a>>>,
+    sources: Vec<Box<dyn Source>>,
 }
 
-impl<'a, T: Ingredient> Pot<'a, T> {
+/// Worker that runs the recipe and brew tea.
+pub struct Brewer<'a> {
+    steps: &'a Vec<Box<dyn Ingredient<'a>>>,
+    tea: Tea,
+}
+
+// TODO: implement Debug for Box<dyn Ingredient>
+// impl<'a> fmt::Debug for Brewer<'a> {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, 
+//                "Brewer {{ steps: {:?}, tea: {:?} }}", 
+//                self.steps.iter().map(|step| &*step), 
+//                self.tea
+//                )
+//     } 
+// }
+
+impl<'a> Pot<'a> {
     ///
     /// Initializes Pot with an empty recipe.
-    pub fn new() -> Pot<'a, T> {
-        Pot { recipe: Vec::new(), tea: Tea::new() }
+    pub fn new() -> Pot<'a> {
+        Pot { recipe: Vec::new(), sources: Vec::new() }
     }
 
     ///
     /// The ingredient is the instruction being added to the brew.
-    pub fn add(&mut self, ingredient: T) {
+    pub fn add_ingredient(&mut self, ingredient: Box<dyn Ingredient<'a>>) {
         &self.recipe.push(ingredient);
     }
 
-    fn get_tea(&self) -> &'a Tea {
-        &self.tea
+    pub fn add_source(&mut self, source: Box<dyn Source>) {
+        &self.sources.push(source);
     }
 
-    fn fill(&mut self, tea: Tea<'a>) {
-        self.tea = tea;
+    pub fn get_recipe(&self) -> &Vec<Box<dyn Ingredient<'a>>> {
+        &self.recipe
     }
 
     ///
     /// This runs the recipe to transform data.
-    pub fn brew(&self) -> u32 {
-        for i in &self.recipe {
-            if let Some(fill) = i.downcast_ref::<Fill>() {
-                println!("Fill operation!");
-                fill.exec(&self);
-            } else {
-                println!("Some other operation")
+    pub fn brew(&self, mut brewer: Brewer) -> u32 {
+        let init_tea = brewer.get_tea();
+        println!("Initial tea: {:?}", init_tea);
+        for source in self.sources.iter() {
+            let fill = source.as_any().downcast_ref::<Fill>().unwrap();
+            let tea = fill.collect();
+            brewer.update_brew(tea);
+            println!("Tea from source: {:?}", brewer.get_tea());
+        }
+        
+        for step in brewer.steps.iter() {
+            if let Some(steep) = step.as_any().downcast_ref::<Steep>() {
+                println!("Steep operation!");
+                let tea = steep.exec(brewer.get_tea());
+                brewer.update_brew(tea);
+            } else if let Some(pour) = step.as_any().downcast_ref::<Pour>() {
+                println!("Pour operation!");
+                let _tea = pour.exec(brewer.get_tea());
             }
+        }
+
+        for step in self.recipe.iter() {
+            step.print();
         }
         3
     }
 }
 
+impl<'a> Brewer<'a> {
+    pub fn new(steps: &'a Vec<Box<dyn Ingredient<'a>>>) -> Brewer<'a> {
+        let tea = Tea::new();
+        Brewer { steps, tea }
+    }
+    pub fn update_steps(&mut self, steps: &'a Vec<Box<dyn Ingredient<'a>>>) {
+        self.steps = steps;
+    }
+
+    pub fn get_tea(&self) -> &Tea {
+        &self.tea
+    }
+
+    fn update_brew(&mut self, tea: Tea) {
+        self.tea = tea;
+    }
+}
+
 // To be able to pattern match, need to define keys being mapped to from Fill
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub struct RawTea1<'a> {
+pub struct RawTea1 {
     x: i32,
-    str_val: &'a str,
+    str_val: String,
     y: bool,
 }
 
-//#[derive(Debug, PartialEq)]
-/// Data Structure defining types of Ingredients that can be added to the brew.
 ///
-/// Forums online state that "trait Ingredient" is the proper way to handle methods and referencing
-/// components.
-//pub enum Ingredient<'a> {
-//    Fill(FillStruct),
-//    Transfuse(Vec<Tea<'a>>),
-//    Steep,
-//    Skim,
-//    Pour,
-//}
-
-trait Ingredient{
-    fn exec(&self) -> Tea;
+/// Raw data inputs
+pub trait Source {
+    ///
+    /// Currently this outputs Tea, in the future it will pull in all desired data, pushing it in
+    /// batches to a source that the Brewers pull from.
+    fn collect(&self) -> Tea;
+    fn as_any(&self) -> &dyn Any;
+    fn print(&self);
 }
 
-pub struct Fill;
-pub struct Transfuse;
-pub struct Steep;
-pub struct Skim;
-pub struct Pour;
+pub trait Ingredient<'a> {
+    fn exec(&self, tea: &Tea) -> Tea;
+    fn print(&self); 
+    fn as_any(&self) -> &dyn Any;
+}
 
-impl<'a> Ingredient for Fill {
-    fn exec(&self) -> Tea {
+pub struct Fill{
+    pub source: String,
+    pub name: String,
+}
+
+pub struct Transfuse;
+
+pub struct Steep{
+    pub name: String,
+}
+
+pub struct Skim{
+    pub name: String,
+}
+
+pub struct Pour{
+    pub name: String,
+}
+
+impl Source for Fill {
+    fn collect(&self) -> Tea {
         let data = r#"{
-            "x": 1,
-            "str_val": "test",
+            "x": 10000,
+            "str_val": "new_values",
             "y": false
         }"#;
         let data: RawTea1 = serde_json::from_str(data).unwrap();
         Tea { data }
     }
+    fn print(&self) {
+        println!("Current Step: {}", self.name);
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
 
-impl Steep {
-    fn steep(tea: Tea) -> Tea {
-        match tea.data.x {
-            1 => println!("{:?}", 1),
-            _ => println!("Does not have key \"x\"")
-        }
-        tea
+impl<'a> Ingredient<'a> for Steep {
+    fn exec(&self, tea: &Tea) -> Tea {
+        let x = tea.data.x;
+        let x = x - 1234567;
+        let mut new_tea = Tea { data: RawTea1 { x, str_val: String::from("test"), y: false } };
+        new_tea
+
+    }
+    fn print(&self) {
+        println!("Current Step: {}", self.name);
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl<'a> Ingredient<'a> for Pour {
+    fn print(&self) {
+        println!("Current Step: {}", self.name);
+    }
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn exec(&self, tea: &Tea) -> Tea {
+        println!("Dumped tea out! Oops");
+        Tea { data: RawTea1 { x: 1, str_val: String::from("test"), y: false } }
     }
 }
 
 #[derive(Debug, PartialEq)]
 /// Resulting data that is being manipulated in the brew.
-pub struct Tea<'a> {
-    data: RawTea1<'a>
+pub struct Tea {
+    data: RawTea1
 }
 
-impl<'a> Tea<'a> {
+impl Tea {
     /// Temporarily, new creates sample data to test constructing the recipe and adding it to the
     /// Pot. In the future, Fill will result in data that is passed on to the processes to be
     /// brewed.
-    pub fn new() -> Tea<'a> {
+    pub fn new() -> Tea {
         let data = r#"{
-            "x": 1,
-            "str_val": "test",
-            "y": false
+          "x": 1,
+          "str_val": "new_values",
+          "y": false
         }"#;
         let data: RawTea1 = serde_json::from_str(data).unwrap();
         Tea { data }
@@ -131,20 +220,18 @@ mod tests {
     #[test]
     fn create_pot_with_recipe() {
         let mut new_pot = Pot::new();
-        let tea = Tea::new();
-        new_pot.add(Ingredient::Fill);
-        new_pot.add(Ingredient::Steep(&tea));
-        new_pot.add(Ingredient::Pour(&tea));
-        assert_eq!(new_pot.recipe, vec![Ingredient::Fill, Ingredient::Steep(&tea), Ingredient::Pour(&tea)]);
+        new_pot.add(Box::new(Fill));
+        new_pot.add(Box::new(Steep));
+        new_pot.add(Box::new(Pour));
+        assert_eq!(new_pot.recipe, vec![Box::new(Fill), Box::new(Steep), Box::new(Pour)]);
     }
 
     #[test]
     fn brew_recipe() {
         let mut new_pot = Pot::new();
-        let tea = Tea::new();
-        new_pot.add(Ingredient::Fill);
-        new_pot.add(Ingredient::Steep(&tea));
-        new_pot.add(Ingredient::Pour(&tea));
+        new_pot.add(Box::new(Fill));
+        new_pot.add(Box::new(Steep));
+        new_pot.add(Box::new(Pour));
         assert_eq!(new_pot.brew(), 3);
     }
 }
