525b72e4045ec1b820f90afbd34ab543bd3edda7
Benjamin Slater <ben@hackcapital.com>
2019-05-28 22:03:34 -0700

    finished tests for traits used in main

--- src/ingredient.rs
+++ src/ingredient.rs
@@ -111,12 +111,12 @@ impl<'a> Ingredient<'a> for Pour {
 
 #[cfg(test)]
 mod tests {
-    use super::super::ingredient::{Fill, Steep, Pour, Argument};
+    use super::super::ingredient::{Fill, Steep, Pour, Argument, Ingredient};
     use super::super::tea::Tea;
     use super::super::source::Source;
     use std::any::Any;
 
-    #[derive(Debug, PartialEq, Default)]
+    #[derive(Debug, PartialEq, Default, Clone)]
     struct TestTea {
         x: i32,
     }
@@ -146,7 +146,7 @@ mod tests {
         let fill = Fill {
             name: String::from("test_fill"),
             source: String::from("text"),
-            computation: Box::new(|_args| {
+            computation: Box::new(|_args: &Option<Box<dyn Argument>>| {
                 TestTea::new(Box::new(TestTea::default()))
             }),
             params: None,
@@ -159,11 +159,103 @@ mod tests {
         let fill = Fill {
             name: String::from("test_fill"),
             source: String::from("text"),
-            computation: Box::new(|_args| {
+            computation: Box::new(|_args: &Option<Box<dyn Argument>>| {
                 TestTea::new(Box::new(TestTea::default()))
             }),
             params: Some(Box::new(TestArgs { val: 5 })),
         };
         assert_eq!(fill.get_name(), "test_fill");
+    }
+
+    #[test]
+    fn create_steep_no_params() {
+        let steep = Steep {
+            name: String::from("test_steep"),
+            computation: Box::new(|tea: &Box<dyn Tea>, _args: &Option<Box<dyn Argument>>| {
+                let tea = tea.as_any().downcast_ref::<TestTea>().unwrap();
+                let mut new_tea = tea.clone();
+                new_tea.x = tea.x + 5;
+                Box::new(new_tea)
+            }),
+            params: None,
+        };
+        let orig_tea = TestTea::new(Box::new(TestTea::default()));
+        let new_tea = steep.exec(&orig_tea);
+        let orig_tea = orig_tea.as_any().downcast_ref::<TestTea>().unwrap();
+        let new_tea = new_tea.as_any().downcast_ref::<TestTea>().unwrap();
+        assert_eq!(steep.get_name(), "test_steep");
+        assert_eq!(new_tea.x, orig_tea.x + 5);
+    }
+
+    #[test]
+    fn create_steep_with_params() {
+        let steep = Steep {
+            name: String::from("test_steep"),
+            computation: Box::new(|tea: &Box<dyn Tea>, args: &Option<Box<dyn Argument>>| {
+                let tea = tea.as_any().downcast_ref::<TestTea>().unwrap();
+                let mut new_tea = tea.clone();
+                match args {
+                    None => println!("Nothing"),
+                    Some(box_args) => {
+                        let box_args = box_args.as_any().downcast_ref::<TestArgs>().unwrap();
+                        new_tea.x = tea.x + box_args.val;
+                    }
+                }
+                Box::new(new_tea)
+            }),
+            params: Some(Box::new(TestArgs { val: 10 })),
+        };
+        let orig_tea = TestTea::new(Box::new(TestTea::default()));
+        let new_tea = steep.exec(&orig_tea);
+        let orig_tea = orig_tea.as_any().downcast_ref::<TestTea>().unwrap();
+        let new_tea = new_tea.as_any().downcast_ref::<TestTea>().unwrap();
+        assert_eq!(steep.get_name(), "test_steep");
+        assert_eq!(new_tea.x, orig_tea.x + 10);
+    }
+
+    #[test]
+    fn create_pour_no_params() {
+        let pour = Pour {
+            name: String::from("test_pour"),
+            computation: Box::new(|tea: &Box<dyn Tea>, _args: &Option<Box<dyn Argument>>| {
+                let tea = tea.as_any().downcast_ref::<TestTea>().unwrap();
+                let new_tea = tea.clone();
+                println!("Output tea to terminal: {:?}", tea);
+                Box::new(new_tea)
+            }),
+            params: None,
+        };
+        let orig_tea = TestTea::new(Box::new(TestTea::default()));
+        let new_tea = pour.exec(&orig_tea);
+        let orig_tea = orig_tea.as_any().downcast_ref::<TestTea>().unwrap();
+        let new_tea = new_tea.as_any().downcast_ref::<TestTea>().unwrap();
+        assert_eq!(pour.get_name(), "test_pour");
+        assert_eq!(new_tea.x, orig_tea.x);
+    }
+
+    #[test]
+    fn create_pour_with_params() {
+        let pour = Pour {
+            name: String::from("test_pour"),
+            computation: Box::new(|tea: &Box<dyn Tea>, args: &Option<Box<dyn Argument>>| {
+                let tea = tea.as_any().downcast_ref::<TestTea>().unwrap();
+                let new_tea = tea.clone();
+                match args {
+                    None => panic!("No params!"),
+                    Some(box_args) => {
+                        let box_args = box_args.as_any().downcast_ref::<TestArgs>().unwrap();
+                        println!("Output tea to terminal, with param: {:?} {}", tea, box_args.val);
+                    }
+                }
+                Box::new(new_tea)
+            }),
+            params: Some(Box::new(TestArgs { val: 10 })),
+        };
+        let orig_tea = TestTea::new(Box::new(TestTea::default()));
+        let new_tea = pour.exec(&orig_tea);
+        let orig_tea = orig_tea.as_any().downcast_ref::<TestTea>().unwrap();
+        let new_tea = new_tea.as_any().downcast_ref::<TestTea>().unwrap();
+        assert_eq!(pour.get_name(), "test_pour");
+        assert_eq!(new_tea.x, orig_tea.x);
     }
 }
